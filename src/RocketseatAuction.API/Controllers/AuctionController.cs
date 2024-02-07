2c9c34ba952015c96296f4d405f9185c371f45c8
NAVE\julio <julio_andrade11@hotmail.com>
2024-02-07 20:46:54 -0300

    aula 1

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+namespace RocketseatAuction.API.Controllers;
+[Route("[Controller]")]
+[ApiController]
+public class AuctionController:ControllerBase {
+    [HttpGet]
+    [ProducesResponseType(typeof(Auction),StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetCurrentAuction() {
+        var userCase= new GetCurrentAuctionUseCase();
+        var result = userCase.Execute();
+        if (result is null) {
+            return NoContent();
+        }
+        return Ok(result);
+    }
+}
---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext :DbContext {
+    public DbSet<Auction> Auctions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlite(@"Data Source=C:\\Users\\julio\\Downloads\\leilaoDbNLW.db");
+    }
+}
